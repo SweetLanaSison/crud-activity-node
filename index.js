9ee79652e92161d70e2645291cf83063a7584459
SweetLanaSison <sweetlanasison@gmail.com>
2024-09-09 22:04:02 +0800

    change to studentRoutes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', require('./routes/StudentRoutes'));
+app.use('/api', require('./routes/studentRoutes'));
 
 mongoose.connect(process.env.MONGO_URI, {userNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('MongoDB connected'))
